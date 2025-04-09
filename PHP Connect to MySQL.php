e51e6c28d423b240d06db870715072f84ba573c6
hichameait <its.aitbenalla.hichame@gmail.com>
2025-04-09 16:32:43 +0100

    add code html

---
+++ PHP Connect to MySQL.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+
+        <button type="submit" name="connect">connect</button>
+        <p><?php echo $code; ?></p>
+
+    </form>
+</body>
+</html>
